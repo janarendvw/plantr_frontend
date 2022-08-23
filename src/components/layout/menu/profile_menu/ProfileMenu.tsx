09a108643f2853d11fca00a9e4911c33fc7cdd9b
janarendvw <janarenddvw@gmail.com>
2022-08-23 10:28:15 +0200

    added more menu groups

--- src/components/layout/menu/profile_menu/ProfileMenu.tsx
+++ src/components/layout/menu/profile_menu/ProfileMenu.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 
 function ProfileMenu() {
     return (
-        <>
+        <div className='flex flex-col h-full justify-between'>
         <div className='card flex-row gap-4 p-4'>
             <div className="avatar">
                 <div className="w-10 rounded-full">
@@ -15,10 +15,15 @@ function ProfileMenu() {
                 <span className='text-xs opacity-50'>g.wagenaar@gmail.com</span>
             </div>
         </div>
-        <ul className="menu menu-compact mt-3">
+        <ul className="menu md:menu-compact mt-3 card">
             <li>
                 <Link to='general-settings'>
                     <i className='material-symbols-rounded text-base'>settings</i> Algemene instellingen
+                </Link>
+            </li>
+            <li>
+                <Link to='privacy-settings'>
+                    <i className='material-symbols-rounded text-base'>lock</i> Beveiliging & Privacy
                 </Link>
             </li>
             <li>
@@ -26,8 +31,18 @@ function ProfileMenu() {
                     <i className='material-symbols-rounded text-base'>edit</i> Profiel aanpassen
                 </Link>
             </li>
+            <li>
+                <Link to='info'>
+                    <i className='material-symbols-rounded text-base'>info</i> Informatie
+                </Link>
+            </li>
+            <li>
+                <Link to='support'>
+                    <i className='material-symbols-rounded text-base'>support</i> Support
+                </Link>
+            </li>
         </ul>
-        </>
+        </div>
     );
 }
 
--- src/pages/PageProfile.tsx
+++ src/pages/PageProfile.tsx
@@ -4,11 +4,11 @@ import {Outlet} from "react-router-dom";
 
 function PageProfile() {
     return (
-        <div className='grid gap-4 md:grid-cols-3 grid-rows-1 max-w-full'>
-            <div className='card col text-base-content shadow p-4'>
+        <div className='grid gap-y-4 md:gap-x-4 grid-cols-1 md:grid-cols-3 grid-rows-1 w-full m-5 md:w-auto md:max-w-full'>
+            <div className='col text-base-content w-full'>
                 <ProfileMenu/>
             </div>
-            <div className='card col-span-2 border border-base-200/40 border-dashed h-full p-4'>
+            <div className='card col-span-2 h-full border border-base-content/10 w-full'>
                 <Outlet/>
             </div>
         </div>
